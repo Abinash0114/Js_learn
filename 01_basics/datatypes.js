77cbeb96e5b40e99f4386ee0478d0852a9204cff
Abinash0114 <abinash3270@gmail.com>
2025-08-09 09:47:19 +0000

    datatype wala part hogya

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,26 @@
+/*Dekh bhai pehle ke js mai naye features nahi the to files 
+sab default count hote the, to agar koi file mai tab ke time kholna chahunga but
+ i would want to use it as aaj ke version pe or something like
+ that to hum "use script" ka istemaaaaaaaaaalllllllllll kar sakte hain*/
+"use strict"; //to use all codes here as newer version of js
+// alert("Arey benshot") // we are using node js, not browser :refer notes for more info
+
+console.log(3 + 3);  // keep on using semicolon its a professional way [Code readability]
+console.log("Abinash");
+let name = "Abinash"
+let age = 18
+let is = false
+
+// 2^53 range in memory address
+// bigint
+// string --> ""
+// boolean = True/False
+// null --> Standalone value(kuch bhi nahi hai[empty])
+// undefined --> kuch hai par kya hai nahi pata
+// symbol --> to define uniqueness
+
+
+//object 
+
+console.log(typeof undefined);
+
