d475298dd666e30d720d15ed70c8482c5f2b5258
Abinash0114 <abinash3270@gmail.com>
2025-08-09 19:37:41 +0000

    comparision wala part bhi done

---
+++ 01_basics/comparision.js
@@ -0,0 +1,22 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+console.log("2"<1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // converted null to 0, broooo wtf
+console.log(undefined > 0); // same for this mf
+
+console.log(0 === 0); // checks even the datatype
+
+
+
+ 
+
+
+
+
+
