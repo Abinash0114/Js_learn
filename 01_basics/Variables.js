1ad68a643e439437625e82dd07cc8f1c065ea929
Abinash0114 <abinash3270@gmail.com>
2025-08-09 08:38:16 +0000

    Just updated the test.js as variables.js according to the course i m following

---
+++ 01_basics/Variables.js
@@ -0,0 +1,16 @@
+const accountId = 15422
+let accountEmail = "abinashisreal@gmail.com"
+var accountPassword = "16724"
+accountCity = "Bhubaneswar" //without saying its data type we can declare it.
+let accountState
+
+// accountId = 2 // not allowed to change constant values
+accountEmail = "abmaikyabolu@gmail.com"
+accountPassword = "Hojayega bhai"
+accountCity = "Yaan"
+
+/*var ko use nahi karenge, dont prefer it, because of issue in log scope and funcitonal scope*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
