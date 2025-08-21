e4768196bbf64f6d3787916ee330be96079f3f9c
Abinash0114 <abinash3270@gmail.com>
2025-08-21 09:42:29 +0000

    done

--- 01_basics/07_dateandtime.js
+++ 01_basics/07_dateandtime.js
@@ -5,5 +5,18 @@ console.log(mydate.toJSON());
 console.log(mydate.toLocaleString());
 console.log(typeof mydate);
 
+let myCreateDate = new Date(2023, 0, 23);
+console.log(myCreateDate);
+console.log(myCreateDate.toDateString());
+console.log(myCreateDate.toLocaleDateString());
 
+let reateDate = new Date("2023-01-23");
+console.log(reateDate.toLocaleString());
+
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+// console.log(myTimeStamp.getTime());bhulachi
+let newDate = new Date();
+console.log(newDate().getDay());
 
